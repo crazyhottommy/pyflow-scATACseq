f143f6228a3c73dcbd1f1df26e85fdbb0eec64aa
crazyhottommy <tangming2005@gmail.com>
2019-06-18 16:09:14 -0400

    restrict local core N

---
+++ pyflow-scATACseq.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+## make a folder for bsub_log if not exist
+folder="sbatch_log"
+
+
+if [ ! -d "$folder" ]; then
+    mkdir -p "$folder"
+fi
+
+snakemake --jobs 99999 \
+	-k \
+    --local-cores 5 \
+    --latency-wait 240 \
+    --cluster-config cluster.json \
+    --cluster "sbatch --partition {cluster.p} --mem {cluster.mem} -N {cluster.N} -n {cluster.n} --time {cluster.time} --job-name {cluster.name} --output {cluster.output} --error {cluster.error}" \
+    "$@"
+	 
